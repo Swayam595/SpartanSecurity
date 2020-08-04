9b0e5c1e7e1570e6bc10c1b8f2565fb2acf788b0
nikitabairagi <nikita.bairagi@iotium.io>
2020-08-03 21:11:30 -0700

    removed unwanted files

--- app/application.py
+++ app/application.py
@@ -1,22 +1,15 @@
 from flask import Flask, jsonify, request, render_template
-from ModelException import ModelException
-# from ModelManager import ModelManager
-import json
-
 application = app = Flask(__name__)
-# manager = ModelManager()
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/intersection')
 def intersection():
     return render_template('intersection.html')
-
-# @app.route('/hotspots')
-# def hot_spots():
-#     return render_template('hotspots.html')
 
 
 @app.route('/predanalytics')
@@ -52,27 +45,6 @@ def safety():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
-
-# # api to upload data set file
-# @app.route("/api/upload", methods=['POST'])
-# def upload_data():
-#     try:
-#         file = request.files['data']
-#         name = request.form['name']
-#         return jsonify(manager.upload_data(name, file))
-#     except Exception as ex:
-#         return ModelException.to_json(ex), 500
-#
-#
-# # api to create time series model
-# @app.route("/api/generatemodel", methods=['POST'])
-# def generate_model():
-#     try:
-#         year = request.form['year']
-#         return jsonify(manager.generate_model(year))
-#
-#     except Exception as ex:
-#         return ModelException.to_json(ex), 500
 
 
 if __name__ == '__main__':
